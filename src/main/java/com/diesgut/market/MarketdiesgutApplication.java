b96a7302938247baaec0d8f1cfc7496acc4d2d67
Usuario <renato_2608@hotmail.com>
2019-09-14 15:54:48 -0500

    primer commit

---
+++ src/main/java/com/diesgut/market/MarketdiesgutApplication.java
@@ -0,0 +1,13 @@
+package com.diesgut.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketdiesgutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketdiesgutApplication.class, args);
+	}
+
+}
